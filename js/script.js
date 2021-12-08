0dccc1737fbd276493ec5c3a53a311251d97ea26
Mário Monteiro da Costa <59767266+mcmgeomagic1@users.noreply.github.com>
2021-12-09 00:04:24 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 let rating5Stars = document.getElementById("js--5stars")
 let maxAvaregeRatingRevews = 3218
 let ratingReviews = [7,5,50,229,2928]
-let breakdownRatingRow = document.querySelectorAll('.breakdown-rating__row span')
-console.log(breakdownRatingRow)
+let breakdownRatingRow = document.querySelectorAll('.breakdown-rating__row')
+let reviewsSection__breakdownRating = document.querySelector(".reviewsSection__breakdown-rating")
+
 function calcRatingPercentages (typeOfratingReviews, idElementName) {
 	let percentages = (100 * typeOfratingReviews) / 3218
 	document.getElementById(idElementName).value = percentages
@@ -11,3 +12,13 @@ function calcRatingPercentages (typeOfratingReviews, idElementName) {
 for(let i = 4; i > 0; i--) {
 		calcRatingPercentages(ratingReviews[i], 'js--bar'+[i] )
 }
+
+
+reviewsSection__breakdownRating.addEventListener("mouseover", ()=> {
+	reviewsSection__breakdownRating.style.opacity = 0.6
+})
+
+reviewsSection__breakdownRating.addEventListener("mouseout", ()=> {
+	reviewsSection__breakdownRating.style.opacity = 1
+})
+
